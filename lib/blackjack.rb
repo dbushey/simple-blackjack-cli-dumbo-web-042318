7b09c515b5f523b764785971d2aba37f35fc97c4
Daiane Bushey <daianebushey@gmail.com>
2018-04-16 17:11:11 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -56,9 +56,9 @@ end
 def runner
   welcome
   current_total = initial_round
-  until current_total < 21 do
+  until current_total > 21 do
     display_card_total(current_total)
-    hit?(current_total)  
+    hit?(current_total)
   end
   end_game(current_total)
 end
